6807d658c570ae716feaeab0bb1e44043ffc09f3
Samuel Dantas <dantasamuca@gmail.com>
2021-07-15 23:49:47 -0300

    Add operators to sum, subtract and compare clocks

--- clock.rb
+++ clock.rb
@@ -2,12 +2,30 @@ class Clock
   MINUTES_IN_A_HOUR = 60
   HOURS_IN_A_DAY = 24
 
+  attr_accessor :minutes
+
   def initialize(hour: 0, minute: 0)
     @minutes = hour * MINUTES_IN_A_HOUR + minute
   end
 
   def to_s
     '%02i:%02i' % [hour_from_minutes, only_minute]
+  end
+
+  def +(clock)
+    new_clock = Clock.new()
+    new_clock.minutes = @minutes + clock.minutes
+    new_clock
+  end
+
+  def -(clock)
+    new_clock = Clock.new()
+    new_clock.minutes = @minutes - clock.minutes
+    new_clock
+  end
+
+  def ==(clock)
+    to_s == clock.to_s
   end
 
   private
