4228359eb25b9866541d7f6484bb0058e14bc17d
Samuel Dantas <dantasamuca@gmail.com>
2021-10-14 10:49:03 -0300

    Improvments in the clock class

--- clock.rb
+++ clock.rb
@@ -2,39 +2,29 @@ class Clock
   MINUTES_IN_A_HOUR = 60
   HOURS_IN_A_DAY = 24
 
-  attr_reader :minutes
-
   def initialize(hour: 0, minute: 0)
-    @minutes = hour * MINUTES_IN_A_HOUR + minute
+    @total_time_in__minutes = hour * MINUTES_IN_A_HOUR + minute
+    @minute = @total_time_in__minutes % MINUTES_IN_A_HOUR
+    @hour = @total_time_in__minutes / MINUTES_IN_A_HOUR % HOURS_IN_A_DAY
+  end
+
+  def to_minutes
+    @total_time_in__minutes
   end
 
   def to_s
-    '%02i:%02i' % [hour, minute]
+    '%02i:%02i' % [@hour, @minute]
   end
 
   def +(clock)
-    self.class.new(minute: @minutes + clock.minutes)
+    self.class.new(minute: @total_time_in__minutes + clock.to_minutes)
   end
 
   def -(clock)
-    self.class.new(minute: @minutes - clock.minutes)
+    self.class.new(minute: @total_time_in__minutes - clock.to_minutes)
   end
 
   def ==(clock)
     to_s == clock.to_s
-  end
-
-  private
-
-  def hour
-    minutes_to_hours % HOURS_IN_A_DAY
-  end
-
-  def minutes_to_hours
-    @minutes / MINUTES_IN_A_HOUR
-  end
-
-  def minute
-    @minutes % MINUTES_IN_A_HOUR
   end
 end
